42ae25d4f4725c4f3dfe6b7f9f4433cdf220fa1a
TalDahan22 <taldahan04@gmail.com>
2022-06-26 15:02:26 +0300

    patch+delete+query

--- server.js
+++ server.js
@@ -1,34 +1,113 @@
+// import express from "express";
+import { readFile } from "fs";
+// import * as fsp from "fs/promises";
+import express from "express";
 import * as fsp from "fs/promises";
-import * as http from "https";
 
-function readFiles(fileName) {
-  fsp
-    .readFile(fileName, "utf-8")
-    .then((word) => {
-      fsp.readFile(`./translations.json`, "utf-8").then((data) => {
-        const array = JSON.parse(data);
-        const foundWord = array.find((item) => item.english === word);
-        if (foundWord !== undefined) {
-          fsp.writeFile("./he.txt", foundWord.hebrow);
-        }
-      });
-    })
-    .catch((err) => console.log("error", err));
-}
+const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("hi");
+});
 
-// function writeFiles(fileName, data) {
+// app.get("/products", (req, res) => {
 //   fsp
-//     .writeFile(`${fileName}`, `${data}`)
-//     .then(() => console.log("tirgamti"))
-//     .catch((err) => console.log(err));
-// }
+//     .readFile("./products.json", "utf8")
+//     .then((data) => res.send(JSON.parse(data)));
+// });
 
-readFiles("./en.txt");
-// writeFiles("he.txt");
+app.get("/products/:productId", (req, res) => {
+  const { productId } = req.params;
+  fsp.readFile("./products.json", "utf8").then((data) => {
+    const products = JSON.parse(data);
+    const product = products.find((product) => product.id === +productId);
+    console.log(product);
+    res.send(product);
+  });
+});
 
-http
-  .createServer((request, Response) => {
-    readFiles("en.txt");
-    Response.end("success");
-  })
-  .listen(8000);
+function getMaxId(arr) {
+  const ids = arr.map((object) => {
+    return object.id;
+  });
+  const max = Math.max(...ids);
+  return max;
+}
+app.post("/products", (req, res) => {
+  fsp.readFile("./products.json", "utf8").then((data) => {
+    console.log(req.body);
+    const { title } = req.body;
+    console.log(title);
+    if (title) {
+      const products = JSON.parse(data);
+      products.push({
+        id: getMaxId(products) + 1,
+        title: req.body.title,
+        // price: req.body.price,
+      });
+      fsp.writeFile("./products.json", JSON.stringify(products));
+      res.send(products);
+    } else {
+      res.send("show the title of the product");
+    }
+  });
+});
+
+app.patch("/products/:productId", (req, res) => {
+  const { productId } = req.params;
+
+  fsp.readFile("./products.json", "utf8").then((data) => {
+    if (req.body) {
+      const products = JSON.parse(data);
+      const productsIndex = products.findIndex(
+        (product) => product.id === +productId
+      );
+      products[productsIndex] = { ...products[productsIndex], ...req.body };
+      fsp
+        .writeFile("./products.json", JSON.stringify(products))
+        .then(() => {
+          res.send(products);
+        })
+        .catch((err) => "error", products);
+    }
+  });
+});
+
+app.delete("/products/:productId", (req, res) => {
+  const { productId } = req.params;
+
+  fsp.readFile("./products.json", "utf8").then((data) => {
+    if (productId) {
+      const products = JSON.parse(data);
+      const productIndex = products.findIndex(
+        (product) => product.id === +productId
+      );
+      if (productIndex >= 0) {
+        products.splice(productIndex, 1);
+        fsp.writeFile("./products.json", JSON.stringify(products)).then(() => {
+          res.send(products);
+        });
+      } else {
+        res.send(products);
+      }
+    }
+  });
+});
+
+app.get("/products", (req, res) => {
+  console.log("req.query", req.query);
+  fsp.readFile("./products.json", "utf8").then((data) => {
+    const products = JSON.parse(data);
+    if (req.query) {
+      const { title } = req.query;
+      const afterFilterProducts = products.filter((product) =>
+        product.title.toLowerCase().includes(title.toLowerCase())
+      );
+      res.send(afterFilterProducts);
+    } else {
+      res.send(products);
+    }
+  });
+});
+
+app.listen(8000);
